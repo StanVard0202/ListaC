5176cda0f184614f7f5e7491db6aa9e54961fe1b
StanVard0202 <you@example.com>
2022-11-20 20:31:19 +0000

    BIG commit

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
+const requestURL_jogos = "https://raw.githubusercontent.com/StanVard0202/ListaC/Database/jogos.json"
+const requestURL_equipas = "https://raw.githubusercontent.com/StanVard0202/ListaC/Database/equipas.json"
+const request_jogos = new Request(requestURL_jogos)
+const request_equipas = new Request(requestURL_equipas)
+
+const div = document.querySelector("#equipas")
 const table = document.querySelector("#table")
 const width = window.innerWidth
 
-const requestURL = "https://raw.githubusercontent.com/StanVard0202/ListaC/Database/jogos.json"
-const request = new Request(requestURL)
+let handler = 0
 
-window.addEventListener("load", get_data(true))
+window.addEventListener("load", get_data())
 
-setTimeout(get_data(),300000)
+if(handler >= 2){
+    setTimeout(get_data(),300000)
+}
+
 
 function get_data(){
-    fetch(request).then((response) => response.json()).then((data) => print_data(data))
+    fetch(request_jogos).then((response) => response.json()).then((data) => print_data(data))
+    fetch(request_equipas).then((response) => response.json()).then((data) => print_equipas(data))
+    handler++
 }
 
 function print_data(d){
-    console.log(d)
-    db = d
     var html = ""
     for(var i=0;i<d.length;i++){
         html+="<tr>"
@@ -31,4 +39,19 @@ function print_data(d){
 function pontos(team){
     var pontos = (team["vitorias"] * 3) + team["empates"]
     return pontos;
+}
+
+function print_equipas(g){
+    var html = ""
+    for(var i=0;i<g.length;i++){
+        html+="<div class='geral _" + i + "'>"
+        html+="<p class='titulo'>"+ g[i].name +"</p>"
+        html+="<p class='ano'>Ano/Turma: "+ g[i].ano +"</p>"
+        html+="<ul>"
+        for(var a = 0;a<g[i].participantes.length;a++){
+            html+="<li>"+ g[i].participantes[a] +"</li>"
+        }
+        html+="</ul></div>"
+    }
+    div.innerHTML = html
 }
